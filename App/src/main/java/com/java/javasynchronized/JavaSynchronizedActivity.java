aef81cb57b45411614b351f072b0f508157c6dd0
DesignQu <250533855@qq.com>
2018-05-21 09:58:48 +0800

    无

---
+++ App/src/main/java/com/java/javasynchronized/JavaSynchronizedActivity.java
@@ -0,0 +1,44 @@
+package com.java.javasynchronized;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.java.javadesignpatterns.code.code1.SendFactory;
+import com.java.javadesignpatterns.code.code1.Sender;
+import com.java.javadesignpatterns.code.code10.Bridge;
+import com.java.javadesignpatterns.code.code10.MyBridge;
+import com.java.javadesignpatterns.code.code10.SourceSub101;
+import com.java.javadesignpatterns.code.code10.SourceSub102;
+import com.java.javadesignpatterns.code.code10.Sourceable10;
+import com.java.javadesignpatterns.code.code2.Singleton;
+import com.java.javadesignpatterns.code.code3.MyBuilder;
+import com.java.javadesignpatterns.code.code4.Prototype;
+import com.java.javadesignpatterns.code.code5.AdapterTest;
+import com.java.javadesignpatterns.code.code5.Targetable;
+import com.java.javadesignpatterns.code.code6.SourceSub1;
+import com.java.javadesignpatterns.code.code6.SourceSub2;
+import com.java.javadesignpatterns.code.code6.Sourceable;
+import com.java.javadesignpatterns.code.code7.Decorator;
+import com.java.javadesignpatterns.code.code7.Source;
+import com.java.javadesignpatterns.code.code7.Sourceable7;
+import com.java.javadesignpatterns.code.code8.Proxy8;
+import com.java.javadesignpatterns.code.code8.Sourceable8;
+import com.java.javadesignpatterns.code.code9.Computer;
+
+import java.io.IOException;
+
+/**
+ * Java synchronized 用法小结
+ */
+public class JavaSynchronizedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+}
