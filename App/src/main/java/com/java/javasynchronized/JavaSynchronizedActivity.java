54f70dd4181433a44ae593a96b121446b762d07f
DesignQu <250533855@qq.com>
2018-05-21 15:45:42 +0800

    无

--- App/src/main/java/com/java/javasynchronized/JavaSynchronizedActivity.java
+++ App/src/main/java/com/java/javasynchronized/JavaSynchronizedActivity.java
@@ -40,40 +40,124 @@ public class JavaSynchronizedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // 修饰代码块
-        SyncThread syncThread = new SyncThread();
-        Thread thread1 = new Thread(syncThread, "SyncThread1");
-        Thread thread2 = new Thread(syncThread, "SyncThread2");
-        thread1.start();
-        thread2.start();
+//        {
+//            SyncRunnable runnable = new SyncRunnable();
+//            Thread thread1 = new Thread(runnable);
+//            Thread thread2 = new Thread(runnable);
+//            thread1.start();
+//            thread2.start();
+//
+//            // 把上面代码改成下面这样的
+//            // 发现thread3和thread4同时在执行。这是因为synchronized只锁定对象，每个对象只有一个锁（lock）与之相关联
+//            // 这时会有两把锁分别锁定syncThread1对象和syncThread2对象，而这两把锁是互不干扰的，不形成互斥，所以两个线程可以同时执行。
+//            SyncRunnable runnable1 = new SyncRunnable();
+//            SyncRunnable runnable2 = new SyncRunnable();
+//            Thread thread3 = new Thread(runnable1);
+//            Thread thread4 = new Thread(runnable2);
+//            thread3.start();
+//            thread4.start();
+//
+//            Counter account = new Counter("Counter", 1000);
+//            CounterOperator counterOperator = new CounterOperator(account);
+//            for (int i = 0; i < 5; i++) {
+//                Thread thread = new Thread(counterOperator, "Thread" + i);
+//                thread.start();
+//            }
+//        }
 
-        // 把上面代码改成下面这样的
-        // 发现thread3和thread4同时在执行。这是因为synchronized只锁定对象，每个对象只有一个锁（lock）与之相关联
-        // 这时会有两把锁分别锁定syncThread1对象和syncThread2对象，而这两把锁是互不干扰的，不形成互斥，所以两个线程可以同时执行。
-        SyncThread syncThread1 = new SyncThread();
-        SyncThread syncThread2 = new SyncThread();
-        Thread thread3 = new Thread(syncThread1, "SyncThread1");
-        Thread thread4 = new Thread(syncThread2, "SyncThread2");
-        thread3.start();
-        thread4.start();
+        // 修饰静态方法
+        {
+            SyncStaticRunnable runnable1 = new SyncStaticRunnable();
+            SyncStaticRunnable runnable2 = new SyncStaticRunnable();
+            Thread thread3 = new Thread(runnable1);
+            Thread thread4 = new Thread(runnable2);
+            thread3.start();
+            thread4.start();
+        }
+
     }
 
     // 修饰代码块
-    class SyncThread implements Runnable {
-        private int count;
-
-        public SyncThread() {
-            count = 0;
-        }
+    class SyncRunnable implements Runnable {
 
         public void run() {
             synchronized (this) {// 可以去掉此行代码，查看不同的结果
-                for (int i = 0; i < 5; i++) {
+                for (int i = 0; i < 3; i++) {
                     try {
-                        Logg.e(Thread.currentThread().getName() + ":" + (count++));
+                        Logg.e(i);
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                }
+            }
+        }
+    }
+
+    class Counter {
+
+        String name;
+        int count;
+
+        public Counter(String name, int count) {
+            this.name = name;
+            this.count = count;
+        }
+
+        public void add(int count) {
+            this.count += count;
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void sub(int count) {
+            this.count -= count;
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    class CounterOperator implements Runnable {
+
+        private final Counter counter;
+
+        public CounterOperator(Counter counter) {
+            this.counter = counter;
+        }
+
+        public void run() {
+            synchronized (counter) {// 可以去掉此行代码，查看不同的结果
+                counter.add(500);
+                counter.sub(500);
+                Logg.e(Thread.currentThread().getName() + ":" + counter.getCount());
+            }
+        }
+    }
+
+    // 修饰代码块
+    static class SyncStaticRunnable implements Runnable {
+
+        public synchronized void run() {
+            method();
+        }
+
+        public synchronized static void method() {
+            for (int i = 0; i < 3; i++) {
+                try {
+                    Logg.e(i);
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
